9f600a851d8b98b76e0f019179ef149276de7717
Vilhelm Fontell <ville@fontell.se>
2024-11-25 23:23:39 +0100

    lade till importer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import talkWithGraph from './modules/talkWithGraph.js';
 import handleFile from './modules/handleFile.js';
-import makeAuthorChecks from './makeAuthorChecks.js';
+import makeAuthorChecks from './modules/makeAuthorChecks.js';
+import makePreparingChecks from './modules/makePreparingChecks.js';
 
 setInterval(async() => {
    const [token, newRefreshToken] = await talkWithGraph.getToken();
@@ -13,6 +14,7 @@ setInterval(async() => {
    if (jsonHasChanged) {
       console.log('fil har ändrats');
       makeAuthorChecks.checkAuthors(json);
+      makePreparingChecks.checkLanguages(json);
    } else {
       console.log('fil har inte ändrats');
    }
